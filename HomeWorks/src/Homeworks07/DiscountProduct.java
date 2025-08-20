c39d4ca906c188269473a29f1782543207c10e17
elenamikhaylova85 <elenamikhaylova1985@gmail.com>
2025-08-20 08:35:04 +0300

    Add homework7

---
+++ HomeWorks/src/Homeworks07/DiscountProduct.java
@@ -0,0 +1,20 @@
+package Homeworks07;
+
+import java.time.LocalDate;
+
+public class DiscountProduct extends Product {
+    private LocalDate end_date; // дата окончания действия скидки
+
+    //конструктор
+    public DiscountProduct(String name, double price, LocalDate end_date) {
+        super(name,price);
+        this.end_date = end_date;
+        }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+
+}
+
